e685f08b65c1dfc9e056ec36139cedf0594461c1
al-shaimon <alshaimon968@gmail.com>
2023-12-25 00:05:12 +0600

    Login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
-import { useContext, useEffect, useState } from 'react';
-import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { useContext, useState } from 'react';
+// import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import ReCAPTCHA from 'react-google-recaptcha';
 import { AuthContext } from '../../providers/AuthProvider';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
@@ -9,18 +10,18 @@ import bg from '../../assets/others/authentication.png';
 import logo from '../../assets/others/authentication2.png';
 
 const Login = () => {
-  const [disabled, setDisabled] = useState(true);
-
+  // const [disabled, setDisabled] = useState(true);
   const { signIn } = useContext(AuthContext);
 
   const navigate = useNavigate();
   const location = useLocation();
-
   const from = location.state?.from?.pathname || '/';
 
-  useEffect(() => {
-    loadCaptchaEnginge(6);
-  }, []);
+  const [capVal, setCapVal] = useState(null);
+
+  // useEffect(() => {
+  //   loadCaptchaEnginge(6);
+  // }, []);
 
   const handleLogin = (event) => {
     event.preventDefault();
@@ -31,15 +32,6 @@ const Login = () => {
     signIn(email, password).then((result) => {
       const user = result.user;
       console.log(user);
-      // Swal.fire({
-      //   title: 'User Login Successful',
-      //   showClass: {
-      //     popup: 'animate__animated animate__fadeInDown',
-      //   },
-      //   hideClass: {
-      //     popup: 'animate__animated animate__fadeOutUp',
-      //   },
-      // });
       Swal.fire({
         position: 'center',
         icon: 'success',
@@ -51,91 +43,84 @@ const Login = () => {
     });
   };
 
-  const handleValidateCaptcha = (e) => {
-    const user_captcha_value = e.target.value;
-    if (validateCaptcha(user_captcha_value)) {
-      setDisabled(false);
-    } else {
-      setDisabled(true);
-    }
-  };
+  // const handleValidateCaptcha = (e) => {
+  //   const user_captcha_value = e.target.value;
+  //   if (validateCaptcha(user_captcha_value)) {
+  //     setDisabled(false);
+  //   } else {
+  //     setDisabled(true);
+  //   }
+  // };
 
   return (
     <>
       <Helmet>
         <title>Bistro Boss | Login</title>
       </Helmet>
-      <div
+      <section
         style={{
           backgroundImage: `url(${bg})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
         }}
-        className="hero min-h-screen"
+        className="lg:hero min-h-screen"
       >
-        <div className="hero-content flex-col md:flex-row-reverse">
-          <div className="text-center md:w-1/2 lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-              exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-            </p>
-          </div>
-          <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
-            <form onSubmit={handleLogin} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="password"
-                  className="input input-bordered"
-                />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
-                </label>
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <LoadCanvasTemplate />
-                </label>
-                <input
-                  onBlur={handleValidateCaptcha}
-                  type="text"
-                  name="captcha"
-                  placeholder="type the captcha above"
-                  className="input input-bordered"
-                />
-              </div>
-              <div className="form-control mt-6">
-                <input disabled={false} className="btn btn-primary" type="submit" value="Login" />
-              </div>
-            </form>
-            <p>
-              <small>
-                New Here? <Link to="/signUp">Create an Account</Link>
-              </small>
-            </p>
-            <SocialLogin></SocialLogin>
+        <div className="shadow-[10px_10px_10px_10px_rgba(0,0,0,0.25)] lg:mt-5 lg:mb-10">
+          <div className="hero-content flex-col lg:flex-row lg:gap-48">
+            <div className="flex-1">
+              <img className="" src={logo} alt="login" />
+            </div>
+            <div className="flex-1 w-full">
+              <form className="" onSubmit={handleLogin}>
+                <h4 className="text-center text-black font-bold text-2xl">Login</h4>
+                <div>
+                  <label className="label">Email</label>
+                  <input className="input" type="email" name="email" placeholder="Email" />
+                </div>
+                <div>
+                  <label className="label">Password</label>
+                  <input className="input" type="password" name="password" placeholder="Password" />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">
+                      Forget password?
+                    </a>
+                  </label>
+                </div>
+                <div>
+                  {/* <label className="label">
+                    <LoadCanvasTemplate />
+                  </label>
+                  <input
+                    onBlur={handleValidateCaptcha}
+                    className="input max-w-xs"
+                    type="text"
+                    name="captcha"
+                    placeholder="Type here"
+                  /> */}
+                  <ReCAPTCHA
+                    sitekey={import.meta.env.VITE_reCAPTCHA_Site_Key}
+                    onChange={(val) => setCapVal(val)}
+                  />
+                </div>
+                <div>
+                  <input
+                    disabled={!capVal}
+                    className="btn bg-[#D1A054]"
+                    type="submit"
+                    value="Login"
+                  />
+                </div>
+                <p>
+                  <small>
+                    New Here? <Link to="/signUp">Create an Account</Link>
+                  </small>
+                </p>
+                <div className="h-1">
+                  <SocialLogin></SocialLogin>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
